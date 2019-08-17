845d85fdc13250a2ba468b773036dd0f1319f54b
Your Name <rsmith5901@gmail.com>
2019-08-17 19:13:04 -0400

    asfd asfd sadf sadf sdf sd

--- Frontend/src/app/app.component.ts
+++ Frontend/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { OAuthService, JwksValidationHandler } from 'angular-oauth2-oidc';
+import { authConfig } from './auth.config';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,39 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'auth-angular';
+
+  constructor(private oauthService: OAuthService) {
+    this.setup();
+  }
+
+  setup() {
+    this.oauthService.configure({
+      responseType: 'token id_token',
+      logoutUrl: 'http://localhost:4200/',
+      issuer: 'https://login.microsoftonline.com/robdir.onmicrosoft.com/',
+      strictDiscoveryDocumentValidation: false,
+      tokenEndpoint: 'https://login.microsoftonline.com/3040589c-8fcb-484f-a4b7-41a99dc4292c/oauth2/v2.0/token',
+      loginUrl: 'https://login.microsoftonline.com/3040589c-8fcb-484f-a4b7-41a99dc4292c/oauth2/v2.0/authorize',
+      redirectUri: 'http://localhost:4200/callback',
+      clientId: 'e68c25e6-2d2e-453e-bd42-1c4c76bfd41b',
+      dummyClientSecret: '<secret>',
+      scope: '',
+      skipIssuerCheck: true,
+      oidc: true
+    });
+    this.oauthService.setStorage(sessionStorage);
+
+    this.oauthService.tryLogin({});
+  }
+
+  logout() {
+    this.oauthService.logOut();
+  }
+  login() {
+    this.oauthService.initImplicitFlow();
+  }
+
+  public get name() {
+    return 'yay';
+  }
 }
--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -1,18 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { OAuthModule } from 'angular-oauth2-oidc';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+import { CallbackComponent } from './callback/callback.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, CallbackComponent],
+  imports: [HttpClientModule, OAuthModule.forRoot(), BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
